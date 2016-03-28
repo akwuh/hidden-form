01c1fc672ba921a869b0debe23449b3c7d13cc04
jakwuh <jakwuh@gmail.com>
2016-03-28 23:42:28 +0300

    Add support for nested data

--- hidden-form.js
+++ hidden-form.js
@@ -1,18 +1,29 @@
+function combine(name, parent) {
+	return parent ? parent + '[' + name + ']' : name
+}
+
+function populate(form, data, parent) {
+	for (var name in data) {
+		if (!data.hasOwnProperty(name)) continue
+		if (typeof data[name] == 'object') {
+			populate(form, data[name], combine(name, parent))
+		} else {
+			var input = document.createElement('input')
+			input.setAttribute('type', 'hidden')
+			input.setAttribute('name', combine(name, parent))
+			input.setAttribute('value', data[name])
+			form.appendChild(input)
+		}
+	}
+}
+
 function submit(url, method, data) {
 	data = data || {}
 	var form = document.createElement('form')
 	form.setAttribute('action', url || '')
 	form.setAttribute('method', method || 'get')
 	form.style.display = 'none'
-	for (var name in data) {
-		if (data.hasOwnProperty(name)) {
-			var input = document.createElement('input')
-			input.setAttribute('type', 'hidden')
-			input.setAttribute('name', name)
-			input.setAttribute('value', data[name])
-			form.appendChild(input)
-		}
-	}
+	populate(form, data)
 	document.body.appendChild(form)
 	form.submit()
 }
